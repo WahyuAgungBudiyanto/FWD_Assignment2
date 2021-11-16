f6a5fbe7428c6db0e29715dccb878b28e8edafbe
WahyuAgungBudiyanto <s11910102@student.unklab.ac.id>
2021-11-16 22:47:01 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import Todos from "./components/todos/Todos";
+import uuid from "uuid";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+function App() {
+  return (
+    <div>
+      <Todos />
+    </div>
+  );
+}
+
+export default App;
